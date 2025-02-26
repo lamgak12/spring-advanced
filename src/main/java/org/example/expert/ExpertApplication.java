472f0fb67b9e8de061fca0e9c8baa1382bf26551
lamgak12 <tntnqk12@naver.com>
2025-02-27 03:13:02 +0900

    .env적용

--- src/main/java/org/example/expert/ExpertApplication.java
+++ src/main/java/org/example/expert/ExpertApplication.java
@@ -1,5 +1,6 @@
 package org.example.expert;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -11,6 +12,11 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 public class ExpertApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
         SpringApplication.run(ExpertApplication.class, args);
     }
 
